0639d84504ea28ccec4cb9dd30fee4ac51e5dd86
Lossfelt <levin.lossfelt@gmail.com>
2020-01-02 12:43:42 +0100

    jobber på placeArmor

--- src/Functions/funcPlaceArmorCard.js
+++ src/Functions/funcPlaceArmorCard.js
@@ -1,4 +1,5 @@
 export function placeArmorCard(hvilkenKnapp) {
+  let sub = this;
   if (
     this.state.deck[0].value < 10 &&
     this.state.cardsInPlay[hvilkenKnapp][0].picture === "\u{1F0F1}" &&
@@ -8,7 +9,7 @@ export function placeArmorCard(hvilkenKnapp) {
       .picture !== "\u{1F0A0}" &&
     this.state.deck[0].value +
     this.state.cardsInPlay[hvilkenKnapp.replace("Armor", "Royal")][0].value <=
-    20 && checkArmorElegibility(hvilkenKnapp)
+    20 && checkArmorElegibility(hvilkenKnapp, sub) === true
   ) {
     var kortstokk = this.state.deck;
     var bunke = this.state.cardsInPlay[hvilkenKnapp];
@@ -18,26 +19,28 @@ export function placeArmorCard(hvilkenKnapp) {
     this.setState({ hvilkenKnapp: bunke });
   }
 }
-
-function checkArmorElegibility(currentRoyal) {
+//denne virker ikke. Hvis du har en knekt av feil suit og en dame av riktig suit, så vil ingen være gyldig da den ikke setter noe krav høyere enn det andre.
+function checkArmorElegibility(hvilkenKnapp, sub) {
+  let currentRoyal = hvilkenKnapp.replace("Armor", "Royal");
+  let isElegible = true;
   const allRoyals = ["upperLeftRoyal", "upperMiddleRoyal", "upperRightRoyal",
     "leftUpperRoyal", "leftMiddleRoyal", "leftBottomRoyal",
     "rightUpperRoyal", "rightMiddleRoyal", "rightBottomRoyal",
     "bottomLeftRoyal", "bottomMiddleRoyal", "bottomRightRoyal"];
   allRoyals.forEach(royal => {
     if (currentRoyal !== royal) {
-      if (this.state.cardsInPlay[royal][0] !== "\u{1F0F1}" && this.state.cardsInPlay[royal][0] !== "\u{1F0A0}") {
-        if (this.state.cardsInPlay[currentRoyal][0].value > this.state.cardsInPlay[royal][0].value) {
-          return false;
-        } else if (this.state.cardsInPlay[royal][0].suit === this.state.deck[0].suit &&
-          this.state.cardsInPlay[currentRoyal][0].suit !== this.state.deck[0].suit) {
-          return false;
-        } else if (this.state.cardsInPlay[royal][0].color === this.state.deck[0].color &&
-          this.state.cardsInPlay[currentRoyal][0].color !== this.state.deck[0].color) {
-          return false;
+      if (sub.state.cardsInPlay[royal][0] !== "\u{1F0F1}" && sub.state.cardsInPlay[royal][0] !== "\u{1F0A0}") {
+        if (sub.state.cardsInPlay[currentRoyal][0].value > sub.state.cardsInPlay[royal][0].value) {
+          isElegible = false;
+        } else if (sub.state.cardsInPlay[royal][0].suit === sub.state.deck[0].suit &&
+          sub.state.cardsInPlay[currentRoyal][0].suit !== sub.state.deck[0].suit) {
+          isElegible = false;
+        } else if (sub.state.cardsInPlay[royal][0].color === sub.state.deck[0].color &&
+          sub.state.cardsInPlay[currentRoyal][0].color !== sub.state.deck[0].color) {
+          isElegible = false;
         }
       }
     }
   })
-  return true;
+  return isElegible;
 }
