b33c2efce18d8158ac06f4b6048a549bd8a26f27
Lossfelt <levin.lossfelt@gmail.com>
2020-01-15 10:18:46 +0100

    added task

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ Create function to display message if you lost the game
 Disable placement and usage of cards when all royals are dead
 Make a restart-button so you don't have to reload the site to start again
 Change all buttons to just be <div>'s with onClick
+Remove all bottom-placeholder-cards and replace with unicode shield or crown, or letters A or J
 Clean up grid and add pictures/colors to show where different cards are to be placed
 Ensure other cards can't be placed when using joker or ace
 Change stack-length number from static to onHover
